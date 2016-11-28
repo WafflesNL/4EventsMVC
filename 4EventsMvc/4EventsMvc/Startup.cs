e6c1f6225e352bfe2cd3ecd8ec4890599ebf8e37
WafflesNL <seltenbart@gmail.com>
2016-11-28 15:27:37 +0100

    Mvc

---
+++ 4EventsMvc/4EventsMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4EventsMvc.Startup))]
+namespace _4EventsMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
