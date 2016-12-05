d43c7abee96b47986b0d82a983232cb386175664
WafflesNL <seltenbart@gmail.com>
2016-12-05 09:53:48 +0100

    Alle individuele Paginas

---
+++ 4EventsMvc/MvcApplicationEvents/Controllers/MaterialPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationEvents.Controllers
+{
+    public class MaterialPageController : Controller
+    {
+        // GET: MaterialPage
+        public ActionResult MaterialPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ 4EventsMvc/MvcApplicationEvents/Controllers/RentalPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationEvents.Controllers
+{
+    public class RentalPageController : Controller
+    {
+        // GET: Rental
+        public ActionResult RentalPage()
+        {
+            return View();
+        }
+    }
+}
