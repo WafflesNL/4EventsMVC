11852387a39db42d3dc310c0128925f45a9ed2d2
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-12-13 11:01:07 +0100

    Unit test geimplementeerd en database connectie goed werkende

--- 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseEventManagement/DatabaseGetEvent.cs
+++ 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseEventManagement/DatabaseGetEvent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,57 @@ namespace MvcApplicationEvents.Models
 {
     public static class DatabaseGetEvent
     {
+       
         public static List<Event> GetEvents()
         {
             List<Event> EventList = new List<Event>();
 
+            if (DatabaseAcces.OpenConnection())
+            {
+                try
+                {
+                    DatabaseAcces.OpenConnection();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = DatabaseAcces.connect;
+
+                    cmd.CommandText = "SELECT * FROM Event";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        int ID = Convert.ToInt32(reader["ID"]);
+                        string Name = (reader["name"].ToString());
+                        int LocationID = (reader["locationid"] != DBNull.Value) ? Convert.ToInt32(reader["locationid"]) : 0;
+                        Location Location = DatabaseGetlocation.GetLocation(LocationID);
+                        DateTime Datestart = Convert.ToDateTime(reader["datestart"]); 
+                        DateTime Dateend = Convert.ToDateTime(reader["dateend"]);
+                        int Maxvisitors = Convert.ToInt32(reader["maxVisitors"]);
+                        string Description = (reader["description"].ToString()); 
+                  
+                        List<Reservation> ReservationList = null;    //kan ook worden opgehaald met het eventID            
+                        List<Contribution> ContributionList = null;
+
+                        Event Event = new Event(ID, Name, Location, Datestart, Dateend, Maxvisitors, Description, ReservationList, ContributionList );
+                        EventList.Add(Event);
+                    }
+                    return EventList;
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Query Failed: " + e.StackTrace + e.Message.ToString());
+
+                }
+                finally
+                {
+                    DatabaseAcces.CloseConnection();
+                }
+            }
             return EventList;
         }
     }
+
+
+    
 }
+
+
--- 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseLocationManagement/DatabaseGetlocation.cs
+++ 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseLocationManagement/DatabaseGetlocation.cs
@@ -48,7 +48,50 @@ namespace MvcApplicationEvents.Models
 
         }
 
+        public static Location GetLocation(int ID)
+        {     
 
+            if (DatabaseAcces.OpenConnection())
+            {
+
+                try
+                {
+                    DatabaseAcces.OpenConnection();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = DatabaseAcces.connect;
+
+                    cmd.CommandText = "SELECT * FROM LOCATION Where ID = @ID";
+                    cmd.Parameters.Add(new SqlParameter("ID", ID));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        int LocatieID = Convert.ToInt32(reader["ID"]);
+                        string name = (reader["name"].ToString()); 
+                        string street = (reader["street"].ToString()); 
+                        string nr = (reader["nr"].ToString()); 
+                        string postcode = (reader["postcode"].ToString()); 
+                        string place = (reader["place"].ToString());
+
+                        Location Location = new Location(LocatieID, name, street, nr, postcode, place);
+                        return Location;
+                    }
+                                   
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Query Failed: " + e.StackTrace + e.Message.ToString());
+
+                }
+                finally
+                {
+                    DatabaseAcces.CloseConnection();
+                }
+            }
+
+            return null;
+        }
 
 
     }
