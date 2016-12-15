f579203ed2d28eb52bdfb23dac47ef3139cf63f5
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-12-15 14:03:10 +0100

    sdfdsfdsf

--- 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseContributionManagement/DatabaseEditContribution.cs
+++ 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseContributionManagement/DatabaseEditContribution.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,48 @@ namespace MvcApplicationEvents.Models
 {
     public static class DatabaseEditContribution
     {
+        public static bool LikePost(int PostID)
+        {
+            if (DatabaseAcces.OpenConnection())
+            {
+                try
+                {
+                    DatabaseAcces.OpenConnection();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = DatabaseAcces.connect;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.CommandText = "LikePost";
+                    cmd.Parameters.AddWithValue("@PostID", PostID);
+              
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Query Failed: " + e.StackTrace + e.Message.ToString());
+                }
+                finally
+                {
+                    DatabaseAcces.CloseConnection();
+                }
+
+            }
+            return false;
+        }
+
+
+
+
+
+        public static bool ReportPost(int PostID)
+        {
+
+            return true;
+        }
+
+
     }
 }
