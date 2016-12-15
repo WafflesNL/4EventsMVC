918392b9548adc725893059c72958f58aea3b1dd
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-12-15 18:19:57 +0100

    dfdsfdsfds

--- 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseReservationManagement/DatabaseGetReservation.cs
+++ 4EventsMvc/MvcApplicationEvents/Models/DatabaseLayer/DatabaseReservationManagement/DatabaseGetReservation.cs
@@ -11,48 +11,50 @@ namespace MvcApplicationEvents.Models
     {
         public static List<Reservation> GetReservationEvent(int EventID)
         {
-            //List<Reservation> ReservationList = new List<Reservation>();
+            List<Reservation> ReservationList = new List<Reservation>();
 
-            //if (DatabaseAcces.OpenConnection())
-            //{
-            //    try
-            //    {
-            //        DatabaseAcces.OpenConnection();
-            //        SqlCommand cmd = new SqlCommand();
-            //        cmd.Connection = DatabaseAcces.connect;
+            if (DatabaseAcces.OpenConnection())
+            {
+                try
+                {
+                    DatabaseAcces.OpenConnection();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = DatabaseAcces.connect;
 
 
-            //        cmd.CommandText = "select * from RESERVATION rwhere r.EventID = @EventID";
-            //        cmd.Parameters.Add(new SqlParameter("EventID", EventID));
+                    cmd.CommandText = "select * from RESERVATION rwhere r.EventID = @EventID";
+                    cmd.Parameters.Add(new SqlParameter("EventID", EventID));
 
-            //        SqlDataReader reader = cmd.ExecuteReader();
+                    SqlDataReader reader = cmd.ExecuteReader();
 
-            //        while (reader.Read())
-            //        {
-            //            int ID = Convert.ToInt32(reader["ID"]);
-            //            DateTime Begindate = Convert.ToDateTime(reader["datestart"]);
-            //            DateTime Enddate = Convert.ToDateTime(reader["dateend"]);
-            //            bool Paid = Convert.ToBoolean(reader["paid"]);                   
+                    while (reader.Read())
+                    {
+                        int ID = Convert.ToInt32(reader["ID"]);
+                        DateTime datestart = Convert.ToDateTime(reader["datestart"]);
+                        DateTime dateend = Convert.ToDateTime(reader["dateend"]);
+                        bool Paid = Convert.ToBoolean(reader["paid"]);
 
-            //            //Event Event = DatabaseGetEvent
-            //            //List<Account> AccountList = databasegetaccountList
+                        //Place Palce = databasegetplace
 
-            //            Reservation Reservation = new Reservation();
-            //            ReservationList.Add(Reservation);
-            //        }
+                        //Event Event = DatabaseGetEvent
+                        //List<Account> AccountList = databasegetaccountList
 
-            //        return ReservationList;
-            //    }
-            //    catch (SqlException e)
-            //    {
-            //        Console.WriteLine("Query Failed: " + e.StackTrace + e.Message.ToString());
-            //    }
-            //    finally
-            //    {
-            //        DatabaseAcces.CloseConnection();
-            //    }
+                        Reservation Reservation = new Reservation(ID, Paid, datestart, dateend, null, null, null, null);                 
+                        ReservationList.Add(Reservation);
+                    }
 
-            //}
+                    return ReservationList;
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Query Failed: " + e.StackTrace + e.Message.ToString());
+                }
+                finally
+                {
+                    DatabaseAcces.CloseConnection();
+                }
+
+            }
             return null;
         }
 
