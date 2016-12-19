0428e8fad5c32019f72dc83726d989f844081924
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-12-19 14:41:19 +0100

    Event aanmaken werkt

--- 4EventsMvc/MvcApplicationEvents/Controllers/CreateEventController.cs
+++ 4EventsMvc/MvcApplicationEvents/Controllers/CreateEventController.cs
@@ -14,12 +14,12 @@ namespace MvcApplicationEvents.Controllers
         {
             return View();
         }
-        public ActionResult btnCreate(string Name, string description, DateTime DateStart, DateTime Dateend, int Maxvisitors, string location)
+        public ActionResult btnCreate(string Eventname, string Eventdescription, DateTime EventDatestart, DateTime EventDateend, int quantity, string Location)
         {
-            Location Location = new Location(location);
-            Location.GetlocationID();
+            Location L = new Location(Location);
+            L.GetlocationID();
 
-            Event Event = new Event(Name, Location, DateStart, Dateend, Maxvisitors, description);
+            Event Event = new Event(Eventname, L, EventDatestart, EventDateend, quantity, Eventdescription);
 
             if (Event.CreateEvent(Event))
             {
