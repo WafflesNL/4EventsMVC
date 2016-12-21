0392535b9967b0deafd8abd37635734e290a1d84
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-12-21 15:46:05 +0100

    boi

--- 4EventsMvc/MvcApplicationEvents/Controllers/ReservationsPageController.cs
+++ 4EventsMvc/MvcApplicationEvents/Controllers/ReservationsPageController.cs
@@ -13,15 +13,19 @@ namespace MvcApplicationEvents.Controllers
         public ActionResult ReservationsPage(Event Event)
         {
             List<Place> placelist = new List<Place>();
-            for (int i = 0; i < 12; i++)
+            for (int i = 0; i < 40; i++)
             {
                 placelist.Add(new Place(1, 5, 10, false));
             }
             return View(placelist);
         }
 
+        //public ActionResult SeePlace(Place Place)
+        //{
+        //    ViewBag.Place = Place.Number;
 
-
+        //    return View("", );
+        //}
 
         //hiermee maakt gebruiker nieuwe reservering mee aan
         public ActionResult btnCreateReservation(Event Event, Place Place, DateTime dateStart, DateTime dateend)
