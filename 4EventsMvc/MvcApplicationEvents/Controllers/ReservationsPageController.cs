c8f95d4b0d1845760475e088bd832ef1aa9d9e5c
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-12-22 15:13:03 +0100

    RESERVERING WERKT

--- 4EventsMvc/MvcApplicationEvents/Controllers/ReservationsPageController.cs
+++ 4EventsMvc/MvcApplicationEvents/Controllers/ReservationsPageController.cs
@@ -12,7 +12,7 @@ namespace MvcApplicationEvents.Controllers
         // GET: ReservationsPage
         public ActionResult ReservationsPage(Event Event)
         {
-            ViewBag.Place = "nog niets geselecteerd";
+            ViewBag.Place = "geen Plek geselecteerd";
             Event.GetLocation();
             Event.Location.Getplaces();
             return View(Event);
@@ -45,13 +45,56 @@ namespace MvcApplicationEvents.Controllers
                 return RedirectToAction("Home","Home");
             }
             else
-            {
-                return View(); //moet nog fouten afhandeling in
-            }
-
-     
-           
+            {                           
+                Event Event2 = Event.GetEventInformationByname();
+                Event2.GetLocation();
+                Event2.Location.Getplaces();
+                ViewBag.Place = "geen Plek geselecteerd";
+                return View("ReservationsPage", Event2); //moet nog fouten afhandeling in
+            }       
         }
+
+        public ActionResult Sort(int DM, string GOK, Event Event)
+        {       
+                Event Event2 = Event.GetEventInformationByname();
+                Event2.GetLocation();
+                Event2.Location.Getplaces();
+
+                foreach (Place P in Event2.Location.PlaceList)
+                {
+                    if (GOK == "Kleiner dan")
+                    {
+                        if (P.Capacity < DM)
+                        {
+                            P.GrayOut();
+                        }
+                    }
+                    else if (GOK == "Groter dan")
+                    {
+                        if (P.Capacity > DM)
+                        {
+                            P.GrayOut();
+                        }
+                    }
+                }
+
+                ViewBag.title = "geen Plek geselecteerd";
+
+                return View("ReservationsPage", Event2);              
+        }
+
+
+        public ActionResult Free(DateTime datestart, DateTime dateend, Event Event)
+        {
+            Event Event2 = Event.GetEventInformationByname();
+            Event2.GetLocation();
+            Event2.Location.Getplaces();
+
+
+
+            return View("ReservationsPage", Event2);
+        }
+
 
     }
 }
